8f15e96e835a1659fdc2432d0be814605481a51c
Federico <65908512+coval3nte@users.noreply.github.com>
2025-11-27 02:11:57 +0100

    fix

--- Formula/go-xray.rb
+++ Formula/go-xray.rb
@@ -1,8 +1,8 @@
 class GoXray < Formula
   desc "XRay CLI with System-Wide Tunneling"
   homepage "https://github.com/coval3nte/go-xray"
-  head "https://github.com/coval3nte/go-xray.git", branch: "main"
   license ""
+  head "https://github.com/coval3nte/go-xray.git", branch: "main"
 
   depends_on "go" => :build
   depends_on "make" => :build
